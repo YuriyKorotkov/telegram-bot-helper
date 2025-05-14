5ff25165f57cfe5f8dc7e559bae205cf676281fb
YuriyKorotkov <y.korotkov@inbox.ru>
2025-05-14 11:18:25 +0300

    updated the submission

--- reply.py
+++ reply.py
@@ -3,12 +3,12 @@
 import asyncio
 from aiogram import Bot
 
-TOKEN = "7832695484:AAF48z_ifKvpO0Yc9oYiY1QBzQcvYnYiciA"
+TOKEN = "YOUR_TOKEN"
 
 async def main():
     bot = Bot(token=TOKEN)
     try:
-        user_id = 1316342598
+        user_id = "YOUR_ID"
         message = "Здравствуйте, Светлана! Спасибо за обращение. В главном меню перейдите в раздел <<Инструкции для заказщика>> далее в подменю нажмите на <<Добавление фото>> и бот пришлет вам инструкцию"
         await bot.send_message(chat_id=user_id, text=message)
     finally:
